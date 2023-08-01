1a126539d34af61544ae237b08356db5676702f3
serezmatik <57442528+serezmatik@users.noreply.github.com>
2023-08-01 19:24:35 +0200

    fix

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <a href="https://stonehenge.pl">
+          <span className="logo">STONEHENGE</span>
+        </a>
+      </div>
+      <a href="https://serezmatik.github.io/menu/">
+        <button className="language-btn">PL</button>
+      </a>
+    </header>
+  );
+};
+
+export default Header;
